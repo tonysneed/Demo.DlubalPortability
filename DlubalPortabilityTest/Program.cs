b589850d13e1e5212268418d67b18fe3fb4e7fb5
Anthony Sneed <tony.sneed@icloud.com>
2020-11-09 16:54:34 -0600

    Add project files.

---
+++ DlubalPortabilityTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Dlubal.RSTAB8;
+
+namespace DlubalPortabilityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var comment = new Comment
+            {
+                Text = "Hello World!"
+            };
+            Console.WriteLine(comment.Text);
+        }
+    }
+}
